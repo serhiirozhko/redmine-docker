d08fe85ad5e306e99e95b99dadab69ee3289686e
Serhii Rozhko <rozhko.serhii@gmail.com>
2023-10-23 19:02:17 +0000

    Deleted

--- app/redmine_config/additional_environment.rb
+++
@@ -1,2 +0,0 @@
-config.logger = Logger.new(Rails.root.join("log",Rails.env + ".log"),3,5*1024*1024)
-config.logger.level = Logger::INFO
--- scripts/backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Backup db
-docker exec bkp_db_mysql_5.7.42 /usr/bin/mysqldump -u root --password=root_password redmine > /home/pronetware/project/redmine-docker/backup/db/backup_db_redmine_`date +%Y-%m-%d_%H_%M`.sql
-
-# Backup attachments file
-
-rsync -a /home/pronetware/project/redmine-docker/app/data/redmine_data /home/pronetware/project/redmine-docker/backup/app_files
